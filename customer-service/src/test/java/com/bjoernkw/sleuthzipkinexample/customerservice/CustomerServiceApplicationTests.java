3fe55605afb89481a2fba6640387de23cccbd3c1
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-08-14 18:26:29 +0200

    Project structure

---
+++ customer-service/src/test/java/com/bjoernkw/sleuthzipkinexample/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjoernkw.sleuthzipkinexample.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
